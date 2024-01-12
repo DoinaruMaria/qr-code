097da744bf8c4d8f9576d61f21fb9702d6c5c9f8
unknown <bogdan.balan2205@upb.ro>
2024-01-12 15:33:16 +0200

    bileteleMele update

--- resources/views/bileteleMele.blade.php
+++ resources/views/bileteleMele.blade.php
@@ -5,11 +5,8 @@
     </head>
 <body>
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Biletele mele</h2>
-    </x-slot>
-
     <div class="py-12">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Biletele mele</h2>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class=" overflow-hidden">
                @foreach($myTickets as $myTicket)
