08caf8371c5a6f96a902d5c1029116411b52caa2
unknown <bogdan.balan2205@upb.ro>
2024-01-15 16:09:04 +0200

    register and other pages css done

--- resources/views/notTicket.blade.php
+++ resources/views/notTicket.blade.php
@@ -1,7 +1,10 @@
 
 <x-app-layout>
-    <div class="my-20 mx-auto p-10 w-80 bg-white  shadow-sm sm:rounded-lg text-center tracking-wide">
-        <p class="text-3xl">Acest bilet nu exista</p>
+    <div class="my-20 mx-8 md:mx-auto flex flex-col justify-center p-10 w-auto  md:w-80 bg-white   rounded-lg text-center tracking-wide shadow-lg shadow-gray-500 ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="red" class="w-[4rem] h-[4rem] flex justify-center mx-auto ">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+        <p class="text-[24px]  md:text-[32px] pt-4 ">Biletul nu este valid!</p>
     </div>
 </x-app-layout>
 
