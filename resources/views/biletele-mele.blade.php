fd5eca7aa90ea0d3143d81c705d93cb55c40daf1
Maria Doinaru <maria2naru@gmail.com>
2024-01-16 12:15:07 +0200

    url

--- resources/views/biletele-mele.blade.php
+++ resources/views/biletele-mele.blade.php
@@ -8,8 +8,8 @@
                @if($myTicket->event)
                <div id="section">
 
-                <div id="printableSection" class="flex w-full h-[13rem] bg-white">
-                    <div id="ticket_event_cover" class="h-full w-[30%] relative bg-cover bg-center bg-no-repeat" style="background-image: url({{ asset('img/spacefest.png') }}"></div>
+                <div id="printableSection" class="flex w-full h-[13rem] bg-white mb-8">
+                    <div id="ticket_event_cover" class="h-full w-[30%] relative bg-cover bg-center bg-no-repeat" style="background-image: url({{asset($myTicket->event->thumbnail) }}"></div>
                         <div id="info_box" class="w-[45%] p-4 h-full">
                         <div id="content" class="text-black flex flex-col justify-between items-between h-full">
                         <div id="info" class="flex flex-col">
