d5bc332e3e1d5330e5f60dc696d5d0c5ad41d4ca
Ionut Dumitru <ionutdtr@gmail.com>
2024-01-23 17:17:09 +0200

    fixed logo bugs

--- lang/ro/auth.php
+++ lang/ro/auth.php
@@ -16,5 +16,6 @@ return [
     'failed' => 'Aceste date nu corespund cu înregistrările noastre.',
     'password' => 'Parola furnizată este incorectă.',
     'throttle' => 'Prea multe încercări de autentificare. Vă rugăm să încercați din nou în :seconds secunde.',
+    'remember_me' => 'Ține-mă minte',
 
 ];
