9e82edd204fdf5fa8a7e331860d542603eb830d1
avramdaniel23 <125465067+avramdaniel23@users.noreply.github.com>
2024-02-22 12:44:19 +0200

    event page css

--- resources/views/evenimente.blade.php
+++ resources/views/evenimente.blade.php
@@ -114,9 +114,9 @@
             <div class="flex justify-center align-center w-full bg-center bg-no-repeat bg-contain h-[14rem]"
                 style="background-image: url({{ asset($event->logo) }})">
             </div>
-            <div class="text-center font-bold ">
+            <div class="text-center pt-4">
                 @isset($event->excerpt)
-                <h1 class="text-white text-[1.75rem] px-2 lg:px-4">
+                <h1 class="text-white text-[1.75rem] px-2 lg:px-4 font-bold leading-[1.2]">
                     {{$event->excerpt}}
                 </h1>
                 @endisset
@@ -124,7 +124,7 @@
                         <div class="flex justify-center pt-4 text-sm uppercase text-white">
                             <a href="{{ $event->website }}" target="_blank" class="flex justify-center">Viziteaza site-ul
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="ml-2 w-[1.2rem] h-[1.2rem] broder-2 border rounded bg-white text-black">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
+                                  <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
                                 </svg>
                             </a>
                         </div>
@@ -139,7 +139,7 @@
             </div>
             @endisset
             </div>
-                <div class="flex justify-around mb-8 w-fit mx-auto">
+                <div class="flex justify-around mb-8 w-fit mx-auto font-bold">
                     @isset($event->start_date)
                     <h1 class="text-white pr-4">
                         {{ date('d-m-Y', strtotime($event->start_date)) }}
