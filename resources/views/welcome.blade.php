2d08c14abe06ad98257d3f8d4ee4bb2e0d52bdb1
Ionut Vasilache <ionut.vasilache@upb.ro>
2024-02-22 12:46:13 +0200

    welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,11 +26,7 @@
             <div class="flex justify-end text-right ">
                 @auth
                 <a href="{{ url('/acasa') }}"
-                    class="font-semibold p-6 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Acasă</a>
-
-                class="font-semibold py-6 pr-2 flex justify-center items-center text-gray-600 hover:text-gray-900
-                dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm
-                focus:outline-red-500">Acasă</a>
+                    class="font-semibold py-6 pr-2 flex justify-center items-center text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Acasă</a>
 
                 @else
                 <a href="{{ route('login') }}"
@@ -99,7 +95,7 @@
                     <div
                         class="flex w-full mt-[10px] rounded-xl border border-gray-200 dark:border-slate-700 border-[0.5px] justify-between items-center ">
                         <div class="flex flex-col pl-4 py-2 items-left text-sm  ">
-                            <a href="{{ url('evenimente',$event->id) }}">
+                            <a href="{{ url('evenimente',$event->name) }}">
                                 <div class="flex items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor"
@@ -159,7 +155,7 @@
                     <div
                         class="flex w-full mt-[10px] rounded-xl border border-gray-200 dark:border-slate-700 border-[0.5px] justify-between items-center ">
                         <div class="flex flex-col pl-4 py-2 items-left text-sm  ">
-                            <a href="{{ url('evenimente',$event->id) }}">
+                            <a href="{{ url('evenimente',$event->name) }}">
                                 <div class="flex items-center">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor"
@@ -218,7 +214,7 @@
                         <div
                             class="flex w-full mt-[10px] rounded-xl border border-gray-200 dark:border-slate-700 border-[0.5px] justify-between items-center ">
                             <div class="flex flex-col pl-4 py-2 items-left text-sm  ">
-                                <a href="{{ url('evenimente',$event->id) }}">
+                                <a href="{{ url('evenimente',$event->name) }}">
                                     <div class="flex items-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                             stroke-width="1.5" stroke="currentColor"
