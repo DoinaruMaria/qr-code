8c387d54b9d57218c39d9ee213f57a5a590a9a3f
Maria Doinaru <maria2naru@gmail.com>
2024-03-26 15:53:30 +0200

    Link create account in login route

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,12 +34,18 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-                href="{{ route('password.request') }}">
-                {{ __('Ți-ai uitat parola?') }}
-            </a>
-            @endif
+            <div>
+                <a class="mr-3 underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    href="{{ route('register') }}">
+                    {{ __('Creaază cont') }}</a>
+                @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    href="{{ route('password.request') }}">
+                    {{ __('Ți-ai uitat parola?') }}
+                </a>
+                @endif
+            </div>
+
 
             <x-primary-button class="ml-3">
                 {{ __('Log in') }}
