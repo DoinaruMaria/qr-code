f3531792d119bacd0bfaa2d707a87f01edf76b43
BogdanBalan22 <118820339+BogdanBalan22@users.noreply.github.com>
2024-04-11 15:31:28 +0300

    title changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Bilete UPB</title>
+    <title>Bilete UNSTPB</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
